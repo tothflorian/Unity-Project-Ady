e0e329607ab546947283452e76fce180d2271aea
FloriX <tothflorianattila5@gmail.com>
2025-12-09 23:03:35 +0100

    Menu Controller
    Frame for menu controller

---
+++ Assets/Script/MenuController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        //SceneManager.LoadScene("GameScene");
+    }
+
+    public void OptionsMenu(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
