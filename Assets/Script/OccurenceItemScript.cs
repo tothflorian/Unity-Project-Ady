8682607206bc48a9760ea4b4abb2db59db172b9a
Kakaoobab98 <krisszt2312@gmail.com>
2025-12-12 01:13:44 +0100

    GameScene
    GameScene, Scriptable Object, Prefab, Email list

---
+++ Assets/Script/OccurenceItemScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OccurenceItemScript : MonoBehaviour
+{
+    
+    public OccurenceItemScriptableObject data;
+
+    void Start()
+    {
+        
+    }
+}
